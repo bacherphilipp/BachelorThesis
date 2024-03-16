7b689ab7114e413afc9539f459a31752254afd6a
Philipp Bacher <bacherphilipp99@gmail.com>
2024-03-16 22:14:57 +0100

    chore: added license details

--- src/models/hybrid_filtering_deep_learning.py
+++ src/models/hybrid_filtering_deep_learning.py
@@ -13,6 +13,34 @@ import configparser
 from src.models.helpers.user_genre_profile import UserGenreProfileGenerator
 from src.data_retrieval.dbconnect import get_connection
 
+"""
+The implementation of this hybrid filtering model was provied by:
+https://github.com/microsoft-partner-solutions-ai/hybrid-recommender/tree/master
+
+MIT License
+
+Copyright (c) 2020 Microsoft
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+"""
+
 def get_music_data():
     """Retrieves music data containing track features. Can be limites via the data_record_limit setting
 
